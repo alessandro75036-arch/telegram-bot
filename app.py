32f294aaf35464f00c17b11a42b13fe23913ceb7
alessandro75036-arch <alessandro75036@gmail.com>
2026-09-23 23:02:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -30,6 +30,6 @@ def webhook():
     
     return "Nessun dato ricevuto", 400
 
-if __name_ == '__main__':
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
