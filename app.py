2dc8a30bba95122a32adbb9de309dfa0626e20a1
alessandro75036-arch <alessandro75036@gmail.com>
2026-09-23 22:41:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os
 from flask import Flask
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Bot attivo!", 200
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
